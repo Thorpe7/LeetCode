e7b0c56f0c46a83732bc3203ce6f4c6ac04d5344
Thorpe7 <maxwell.thorpe7@gmail.com>
2024-12-25 08:51:45 -0600

    Adding rust code.

--- rust/algorithms/two_sum/src/lib.rs
+++ rust/algorithms/two_sum/src/lib.rs
@@ -25,16 +25,3 @@ impl TwoSum {
         vec![]
     }
 }
-
-fn main() {
-    let nums = vec![2, 7, 11, 15];
-    let target = 9;
-
-    let result = TwoSum::run_two_sum(nums, target);
-
-    if !result.is_empty() {
-        println!("Indices: {:?}", result);
-    } else {
-        println!("No solution found.");
-    }
-}
